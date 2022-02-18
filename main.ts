2cfd87b41e00df8fc9199ffcbbe0a72b0ed42d81
Kurucz István <never@nevergone.hu>
2022-02-18 19:43:13 +0100

    sorompó bekötve szervóval

--- main.ts
+++ main.ts
@@ -1,24 +1,35 @@
+function sarga_lampa () {
+    basic.clearScreen()
+    basic.showString("S")
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(2000)
+    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P1, 0)
+}
 input.onButtonPressed(Button.A, function () {
     if (index == 0) {
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        sarga_lampa()
         pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.servoWritePin(AnalogPin.P8, 0)
         basic.clearScreen()
         basic.showString("P")
-    } else if (index == 1) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.clearScreen()
-        basic.showString("S")
     } else if (index == 2) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
+        sarga_lampa()
+        pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.servoWritePin(AnalogPin.P8, 90)
         basic.clearScreen()
         basic.showString("Z")
     }
-    index += 1
-    if (index == 3) {
+    index += 2
+    if (index >= 3) {
         index = 0
     }
 })
 let index = 0
+basic.clearScreen()
 index = 0
+pins.servoWritePin(AnalogPin.P8, 0)
