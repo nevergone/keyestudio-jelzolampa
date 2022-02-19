38c948ca3d490cea9ce63406390c8bfac64a43a1
Kurucz István <never@nevergone.hu>
2022-02-19 13:23:28 +0100

    inicializálás

--- main.ts
+++ main.ts
@@ -30,6 +30,10 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 let index = 0
-basic.clearScreen()
 index = 0
+pins.digitalWritePin(DigitalPin.P0, 1)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
 pins.servoWritePin(AnalogPin.P8, 0)
+basic.clearScreen()
+basic.showString("P")
